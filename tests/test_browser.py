d249706e1f9a24a378472ee774f232ad496c9489
Damian Rotta <dipiert@gmail.com>
2022-01-30 12:32:29 -0300

    Adding a HTTPURL test

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -14,3 +14,12 @@ class HTTPURLTest(TestCase):
         host, path = self.http_test.get_host_and_path(url)
         self.assertEqual(host, 'example.org')
         self.assertEqual(path, '/')
+
+    def test_get_header_and_body(self):
+        host = "example.org"
+        path = "/"
+        headers, body = self.http_test.get_header_and_body(host, path)
+        # It seems like there is not such a thing as mandatory header fields:
+        # https://stackoverflow.com/a/25586633/5148492
+        self.assertTrue(headers)
+        self.assertTrue("<title>Example Domain</title>" in body)
